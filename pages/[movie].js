03c5243a3d105f175ef2c7bccfa2f7c7b6574d66
Jaiprakash kushwaha <jaiprakash.kushwaha@cogoport.com>
2022-06-06 20:49:27 -0700

    Movie-App

---
+++ pages/[movie].js
@@ -0,0 +1,64 @@
+import { useRouter } from 'next/router'
+import React, { useEffect, useState } from "react";
+const Movie = () => {
+  const router = useRouter()
+  const [movieData, setMovieData] = useState(null);
+  const { movie } = router.query
+  const getMovieRequest = async (resp) => {
+    const url = `http://www.omdbapi.com/?i=${movie}&apikey=3392ffe5`;
+    const response = await fetch(url);
+    const responseJSON = await response.json();
+    console.log(responseJSON);
+    if (responseJSON) {
+        // console.log(responseJSON);
+        setMovieData(responseJSON);
+    }
+  };
+  const setSearchValues = (val) => {
+    setSearchValue(val);
+  };
+  useEffect(() => {
+    const timer = setTimeout(() => {
+        console.log('This will run after 1 second!')
+        getMovieRequest(movie);
+        console.log(movieData, "dddd");
+      }, 1000);
+      return () => clearTimeout(timer);
+    
+  }, []);
+  return (
+      <>
+        {/* <a className='link' href="https://codepen.io/simoberny/pen/qxxOqj" target="_blank">Dark Version</a> */}
+        {
+            movieData  ? 
+            <div className="movie_card" id="bright">
+                <div className="info_section">
+                    <div className="movie_header">
+                    <img className="locandina" src={movieData.Poster}/>
+                    <h1>{movieData.Title}</h1>
+                    <h4>{movieData.Year}</h4>
+                    <span className="minutes">{movieData.Runtime}</span>
+                    <p className="type">{movieData.Genre}</p>
+                    </div>
+                    <div className="movie_desc">
+                    <p className="text">
+                        {
+                            movieData.Plot
+                        }
+                    </p>
+                    </div>
+                    <div className="movie_social">
+                    <ul>
+                        <li><i className="material-icons">Like &hearts;</i></li>
+                    </ul>
+                    </div>
+                </div>
+                <div className="blur_back bright_back"></div>
+                </div>: "Loading..."
+        }
+        
+      </>
+  )
+}
+
+export default Movie
---
+++ pages/index.js
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from "react";
+import Link from 'next/link';
+import Head from "next/head";
+// import '../styles/movie.css'
+
+export default function Home() {
+  const [movies, setMovies] = useState([]);
+  // const [favourites, setFavourites] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const getMovieRequest = async (resp) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=3392ffe5`;
+    const response = await fetch(url);
+    const responseJSON = await response.json();
+    if (responseJSON.Search) {
+      setMovies(responseJSON.Search);
+    }
+  };
+  const setSearchValues = (val) => {
+    setSearchValue(val);
+  };
+  useEffect(() => {
+    getMovieRequest(searchValue);
+  }, [searchValue]);
+  return (
+    <>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Head>
+    <h1>
+      Movie Search Engine
+    </h1>
+      <div className="container-fluid movie-app">
+        <div className="row d-flex align-items-center mt-4 mb-4">
+
+          <div className="col col-sm-4">
+            <input
+              className="form-control search"
+              onChange={(event) => setSearchValues(event.target.value)}
+              value={searchValue}
+              placeholder="Type to search..."
+            ></input>
+              
+            <button>Search</button>
+          </div>
+        </div>
+      </div>
+      <div className="main">
+        {
+          movies.length > 0 ? 
+          <ul className="cards">
+        {
+            movies.map((movie, index) => (
+              <>
+              <li className="cards_item">
+              <div className="card">
+                <div className="card_image">
+                <Link href={`/${movie.imdbID}`} style={{ textDecoration: 'none'}}>
+
+                  <img src={movie.Poster} className="movieImg">  
+                  
+                  </img>
+                  </Link>
+                  
+                  </div>
+                  <div className="card_content">
+                  <h2 className="card_title">{movie.Title}</h2>
+                  <br />
+                  <button className="btn card_btn">
+                  <Link href={`/${movie.imdbID}`} style={{ textDecoration: 'none' }} >
+                      Details 
+                    </Link>
+                  </button>
+                </div>
+              </div>
+            </li>
+              </>
+            ))
+          }
+          
+        </ul>
+        : <h1>No movie found</h1>
+        }
+        
+    </div>
+    </>
+  );
+}
